77385286c4b6a519d87e8c0c1e29b7dc886cca98
Mohd Shadab <ms.shadab06@gmail.com>
2025-01-01 20:24:54 +0530

    date added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,32 @@
 import React from "react";
 
 export const Footer = () => {
+  function getFormattedDate() {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const now = new Date();
+
+    const month = months[now.getMonth()]; // Get month as short name
+    const day = String(now.getDate()).padStart(2, "0"); // Pad day with leading zero if needed
+    const year = now.getFullYear(); // Get full year
+
+    return `${month}, ${day}, ${year}`;
+  }
   return (
     <div className="p-4 text-center justify-center text-xs text-neutral-500 border-t border-neutral-100">
-      <span className="font-semibold">{new Date().getFullYear()} </span>
+      <span className="font-semibold">{getFormattedDate()} </span>
       &#8212; Built by Mohd Shadab
     </div>
   );
